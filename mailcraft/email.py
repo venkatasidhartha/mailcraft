f52e139e2e9325753c191a0af026026209e51b06
venkatasidhartha <venkatasidhartha@gmail.com>
2023-06-03 15:18:13 +0000

    email template and send mail

--- mailcraft/email.py
+++ mailcraft/email.py
@@ -1,25 +1,67 @@
 import frappe
-from mailcraft.email_utility.utility import mailCraft_settings
 import requests
+import json
+import ast
+from mailcraft.email_utility.utility import mailCraft_settings
+from mailcraft.email_utility.template import EmailTemplate
 
-class Email:
-    def __init__(self):
+class _MailCraft_service:
+    def __init__(self) -> None:
         self.settings = mailCraft_settings()
 
-    def render_parent_template(self,body_content):
-        return frappe.render_template(self.settings.parent_template,{"content":body_content})
-    
-    def render_mailcontent(self,template_name,params):
-        template = frappe.get_doc("MailCraft Template",template_name)
-        return frappe.render_template(template.message,params)
-    
-    def send_mail(self,template_name,params):
+    def send_mail(self,to_user:list,params:dict,template_key:str):
+        """
+        to_user = [
+            {
+                "email": "no-reply@karkhana.io",
+                "name": "no-reply"
+            }
+        ]
+        params = {
+            "FIRSTNAME":"no-reply"
+        }
+
+        template_key = "reset_password"
+        """
         if self.settings.switch:
-            mail_content = self.render_mailcontent(template_name,params)
-            final_template = self.render_parent_template(mail_content)
-            print("*"*100)
-            print(final_template)
-            print("*"*100)
+            et = EmailTemplate()
+            template_docname = et.get_template_docname(key=template_key)
+            final_template = et.get_template(template_name=template_docname,params=params)
+
+            url = self.settings.service_url
+            payload = {
+                "sender": {
+                    "email": self.settings.sender_email_address,
+                    "name": self.settings.sender_name
+                },
+                "to": to_user,
+                "htmlContent": final_template,
+                "subject":et.get_subject()
+            }
+            headers = {
+                "accept": "application/json",
+                "content-type": "application/json",
+                "api-key": self.settings.get_password('api_key')
+            }
+            response = requests.post(url, json=payload, headers=headers)
+            if response.status_code != 201:
+                frappe.log_error(title='Mail Request Failed',message=response.text)
 
 
 
+def send_mail(to_user:list,params:dict,template_key:str):
+    """
+        to_user = [
+            {
+                "email": "no-reply@karkhana.io",
+                "name": "no-reply"
+            }
+        ]
+        params = {
+            "FIRSTNAME":"no-reply"
+        }
+
+        template_key = "reset_password"
+        """
+    mail_class = _MailCraft_service()
+    mail_class.send_mail(to_user,params,template_key)
